a8606918e852c3a1156280b65cc67b2fea2703ce
Edwardvee <lautaroivan.quiroga@gmail.com>
2024-09-19 11:40:06 -0300

    feature: GameCard template

--- src/components/CardGameDefault/CardGameDefault.tsx
+++ src/components/CardGameDefault/CardGameDefault.tsx
@@ -1,12 +1,34 @@
-import { Flex, Grid, Card, Col } from "antd"
+import { CheckCircleFilled, PlusCircleFilled } from "@ant-design/icons"
+import { Card } from "antd"
+import { useState } from "react"
+import { Link } from "react-router-dom"
 import styles from "./index.module.scss"
-export function CardGameD(){
-    return(
+
+interface CardComponent{
+    id: number,
+    title: string,
+    imgSrc: string,
+    releaseDate: string,
+    genre: string[],
+    platforms: string[]
+}
+
+export const CardGameD: React.FC<CardComponent>  = ({id, title, imgSrc, releaseDate, genre, platforms}) => {
+    const [favState, setFavState] = useState(false)
+    return (    
         <>
-        <Col xs={24} sm={12} md={12} lg={6} xl={6}>
-        <Card title="Titulo" className={styles.gamecard}>
-        </Card>
-        </Col>
+                <Card hoverable className={styles.gamecard} bordered={false} cover={<img draggable={false}  alt="Cover" src={imgSrc} />}>
+                    <div className={styles.titleHoverContainer}>
+                    <Link draggable={false} to={"/details/" + id} className={styles.titleHover}>{title}</Link> 
+                        {favState ? <CheckCircleFilled className={styles.PlusHover}  onClick={() => {setFavState(false)}} /> : <PlusCircleFilled className={styles.PlusHover} onClick={() => {setFavState(true)}} /> }
+                    </div>
+                    <Card.Meta title={title} className={styles.title} />
+                    <div className={styles.cardcontent}>
+                        <p>Release date: {releaseDate}</p>
+                        <p>Genre: {genre.join(", ")} </p>
+                        <p>Platforms: {platforms.join(", ")}</p>
+                    </div>
+                </Card>
         </>
     )
 }
