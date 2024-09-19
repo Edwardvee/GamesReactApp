b3f1ddc3164f9b792b478e53c21a22d89bf23032
Edwardvee <lautaroivan.quiroga@gmail.com>
2024-09-19 11:37:38 -0300

    feature: Add routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter} from 'react-router-dom'
 import App from '../App'
 import { Homepage } from '../screens/Homepage/Homepage'
+import { GameDetails } from '../screens/GameDetails/GameDetails'
+import { EditGame } from '../screens/EditGame/EditGame'
+import { CreateGame } from '../screens/CreateGame/CreateGame'
 import { Favorites } from '../screens/Favorites/Favorites'
 export const router = createBrowserRouter([
     {
@@ -8,7 +11,10 @@ export const router = createBrowserRouter([
       element: <App />,
       children: [
         {path: "", element: <Homepage/>},
-        {path: "favorites", element: <Favorites/>}
+        {path: "favorites", element: <Favorites/>},
+        {path: "details/:id", element: <GameDetails />},
+        {path: "create", element: <CreateGame/>},
+        {path: "edit/:id", element: <EditGame/>}
   
       ],
     }
