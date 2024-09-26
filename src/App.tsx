db4a9b2e8b437f11f84a42822b84d43bcd3c60fc
Edwardvee <lautaroivan.quiroga@gmail.com>
2024-09-26 10:08:13 -0300

    feat: add a custom theme with ConfigProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import { Outlet } from "react-router-dom"
-import "./scss/_base.scss"
-import { NavbarComp } from "./components/Navbar/Navbar"
+import { Outlet } from "react-router-dom";
+import "./scss/_base.scss";
+import { NavbarComp } from "./components/Navbar/Navbar";
+import { ConfigProvider } from "antd";
 function App() {
   return (
-    <>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#41445a",
+          colorInfo: "#41445a",
+          colorBgBase: "#41445a",
+          colorBorder: "#fff",
+          colorFillContent: "#fff",
+          colorBgContainer: "#5a6474",
+          colorText: "#fff",
+          colorTextPlaceholder: "#fff",
+          wireframe: false,
+        },
+      }}
+    >
       <NavbarComp></NavbarComp>
       <main>
         <Outlet></Outlet>
       </main>
-    </>
-  )
+    </ConfigProvider>
+  );
 }
 
-export default App
+export default App;
