db0a7d291c8f78a4e7122e6d897f4b276ac2931b
Edwardvee <lautaroivan.quiroga@gmail.com>
2024-10-24 12:51:27 -0300

    feat: add filters

--- src/screens/Homepage/Homepage.tsx
+++ src/screens/Homepage/Homepage.tsx
@@ -5,6 +5,10 @@ import styles from "./homepage.module.scss";
 import { GlobalStateService } from "../../services/globalStateService";
 import { GamesUseCases } from "../../useCases/gamesUseCases";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { JSONGamesUseCases } from "../../useCases/JSONGamesUseCases";
+
+import type { SelectProps } from "antd";
+
 export function Homepage() {
   const screens = Grid.useBreakpoint();
   const getColumnSpan = () => {
@@ -23,12 +27,12 @@ export function Homepage() {
       isDragging = true;
       carousel?.classList.add("dragging");
       startX = e.pageX;
-      startScrollLeft = carousel.scrollLeft;
+      startScrollLeft = carousel!.scrollLeft;
     };
     const dragging = (e: any) => {
       if (!isDragging) return;
       const x = e.pageX - startX;
-      carousel.scrollLeft = startScrollLeft - x;
+      carousel!.scrollLeft = startScrollLeft - x;
     };
     const dragStop = () => {
       isDragging = false;
@@ -50,18 +54,76 @@ export function Homepage() {
   const cGames = GlobalStateService.getGames();
   const dGames = GlobalStateService.getDiscoverGames();
   const cGamesPage = GlobalStateService.getGamesPage();
-
+  const gamesItems = GlobalStateService.getItems();
+  const [dLoading, setDLoading] = useState(true);
   useEffect(() => {
-    GamesUseCases.getDiscoverGames(
-      "page_size=15&ordering=released&page=1"
-    ).finally(() => {
+    GamesUseCases.getDiscoverGames(1, {
+      param: "ordering",
+      value: "released",
+    }).finally(() => {
       carousel = document.getElementById("carousel");
-      console.log(dGames);
+      console.log(cGames);
       setDLoading(false);
     });
+    JSONGamesUseCases.getItems();
+    console.log(cGames);
+  }, []);
+  let dataLength = cGames.length;
+
+  const [source, sourceState] = useState("api");
+  const changeSource = (value: string) => {
+    setFiltersOptionState(value);
+    sourceState(value);
+    refreshCatalog();
+  };
+  const [filter, setFilterState] = useState("");
+  const changeFilter = (value: string) => {
+    setFilterState(value);
+    refreshCatalog();
+  };
+  const [filtersOption, setFiltersOptionState] = useState("api");
+  const filtersAPI = [
+    { value: "name", label: "Name" },
+    { value: "-metacritic", label: "Metacritic" },
+    { value: "released", label: "Release date" },
+    { value: "-rating", label: "Rating" },
+    { value: "-added", label: "Added" },
+    { value: "-updated", label: "Updated" },
+  ];
+  const filtersJSON = [{ value: "name", label: "Name" }];
+  const [hasMore, hasMoreState] = useState(true);
+  const refreshCatalog = () => {
+    GlobalStateService.deleteGames();
+    GlobalStateService.setGamesPage(1);
+  };
+
+  const Genres = GlobalStateService.getGenres();
+  const Platforms = GlobalStateService.getPlatforms();
+  useEffect(() => {
+    GamesUseCases.getGenres();
+    GamesUseCases.getPlatforms();
   }, []);
 
-  const [dLoading, setDLoading] = useState(true);
+  const genresOptions: SelectProps["options"] = Genres.map((g) => ({
+    label: g.name,
+    value: g.id,
+  }));
+  const platformsOptions: SelectProps["options"] = Platforms.map((p) => ({
+    label: p.name,
+    value: p.id,
+  }));
+
+  const [genres, genresState] = useState([""]);
+  const [platforms, platformState] = useState([""]);
+
+  const setUpGenres = (value: string[]) => {
+    refreshCatalog();
+    genresState(value);
+  };
+  const setUpPlatforms = (value: string[]) => {
+    refreshCatalog();
+    platformState(value);
+  };
   return (
     <>
       <h1 className={styles.title} style={{ paddingTop: "15px" }}>
@@ -76,6 +138,7 @@ export function Homepage() {
               {dGames.map((game) => (
                 <CardGameD
                   id={game.id}
+                  source={game.source}
                   title={game.title || "No info."}
                   imgSrc={game.image || "src/assets/img/notfound.png"}
                   releaseDate={game.releaseDate || "No info."}
@@ -97,6 +160,7 @@ export function Homepage() {
             marginLeft: "100px",
             textAlign: "center",
             minWidth: "150px",
+            color: "#fff",
           }}
         >
           Order by:
@@ -104,28 +168,83 @@ export function Homepage() {
             placeholder="Filter by"
             style={{ width: 120, marginInline: "20px" }}
             defaultValue={"Name"}
+            maxTagCount={"responsive"}
+            onChange={changeFilter}
+            options={filtersOption == "api" ? filtersAPI : filtersJSON}
+          />
+          <Select
+            defaultValue="api"
+            style={{ width: 120 }}
+            maxTagCount={"responsive"}
+            onChange={changeSource}
             options={[
-              { value: "name", label: "Name" },
-              { value: "releaseDate", label: "Release date" },
-              { value: "rating", label: "Rating" },
+              { value: "api", label: "API" },
+              { value: "json", label: "JSON DB" },
             ]}
+          />
+          <Select
+            mode="multiple"
+            style={{ width: 220, marginInline: "20px" }}
+            allowClear
+            placeholder="Filter genres"
+            onChange={setUpGenres}
+            maxTagCount={"responsive"}
+            options={genresOptions}
+          />
+          <Select
+            mode="multiple"
+            style={{ width: 220 }}
+            allowClear
+            maxTagCount={"responsive"}
+            placeholder="Filter platforms"
+            onChange={setUpPlatforms}
+            options={platformsOptions}
           />
         </span>
 
         <InfiniteScroll
-          dataLength={cGames.length} //This is important field to render the next data
+          dataLength={cGames.length}
           next={() => {
-            const nextPage = cGamesPage + 1;
-            GamesUseCases.getGames("page=" + cGamesPage);
-            GlobalStateService.setGamesPage(nextPage);
+            console.log(hasMore);
+            if (source == "api") {
+              console.log(cGames.length);
+              GamesUseCases.getGames(
+                cGamesPage,
+                {
+                  param: "ordering",
+                  value: filter ? filter : "Updated",
+                },
+                genres.join(","),
+                platforms.join(",")
+              ).then((newGames) => {
+                GlobalStateService.setGamesPage(cGamesPage + 1);
+              });
+            } else {
+              JSONGamesUseCases.GetGames(cGamesPage.toString()).then(
+                (newGames) => {
+                  const totalItems = GlobalStateService.getItems();
+                  console.log(cGames.length);
+                  console.log(totalItems);
+
+                  if (totalItems <= cGames.length) {
+                    hasMoreState(false);
+                    console.log("set false");
+                    console.log(hasMore);
+                  } else {
+                    GlobalStateService.setGamesPage(cGamesPage + 1);
+                  }
+                }
+              );
+            }
           }}
-          hasMore={true}
+          hasMore={hasMore}
+          refreshFunction={refreshCatalog}
           loader={
             <Spin style={{ margin: "20px" }} tip="Loading" size="large"></Spin>
           }
           endMessage={
             <p style={{ textAlign: "center" }}>
-              <b>Yay! You have seen it all</b>
+              <b>That's the end.</b>
             </p>
           }
         >
@@ -139,6 +258,7 @@ export function Homepage() {
               <Col key={game.id} span={getColumnSpan()} style={{ flex: 0 }}>
                 <CardGameD
                   id={game.id}
+                  source={game.source}
                   title={game.title}
                   imgSrc={game.image || "src/assets/img/notfound.png"}
                   releaseDate={game.releaseDate || "No info."}
