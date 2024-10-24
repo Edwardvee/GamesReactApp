4e51116417e38954ec17f3c7dc99a8be9573f2c9
Edwardvee <lautaroivan.quiroga@gmail.com>
2024-10-24 12:53:13 -0300

    feat: add screenshot interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,7 @@ export interface ITags {
   id: number;
   name: string;
 }
+
 export interface IPlatform {
   id: number;
   name: string;
@@ -19,22 +20,38 @@ export interface IGenres {
   id: number;
   name: string;
 }
-export interface IGameDetail {
+export interface IScreenshot {
   id: number;
+  image: string;
+  width: number;
+  height: number;
+}
+export interface IGameDetail {
+  id: string;
   title: string;
   backgroundImage: string;
   about: string;
   releaseDate: string;
   rating: number;
-  genre: IGenres[];
+  genres: IGenres[];
   platforms: IPlatforms[];
   tags: ITags[];
+  screenshots?: IScreenshot[];
+  source: "api" | "json";
 }
 export interface IGameCard {
-  id: number;
+  id: string;
   title: string;
   releaseDate: string;
   genres: IGenres[];
   platforms: IPlatforms[];
   image: string;
+  source: "api" | "json";
+}
+export interface IFormData extends Omit<IGameDetail, "tags"> {
+  tags: number[];
+}
+export interface IFilter {
+  param: string;
+  value: string;
 }
