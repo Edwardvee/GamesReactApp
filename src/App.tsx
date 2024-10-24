30a7521bce4baa2da80bb722cfaae87846c56fae
Edwardvee <lautaroivan.quiroga@gmail.com>
2024-10-24 12:44:37 -0300

    fix: themes

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,20 @@ function App() {
           colorInfo: "#41445a",
           colorBgBase: "#41445a",
           colorBorder: "#fff",
-          colorFillContent: "#fff",
           colorBgContainer: "#5a6474",
           colorText: "#fff",
+          colorBgElevated: "#5a6474",
           colorTextPlaceholder: "#fff",
           wireframe: false,
+        },
+        components: {
+          Message: {
+            colorText: "#5a6474",
+            contentBg: "#5a6474",
+          },
+          Select: {
+            colorText: "rgb(255,255,255)",
+          },
         },
       }}
     >
